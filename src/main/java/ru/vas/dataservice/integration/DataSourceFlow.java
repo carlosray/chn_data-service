dac4212cfce3699b2a00d2033008d862a1c4259d
carlosray <carlos.ray@mail.ru>
2021-01-05 14:13:55 +0300

    Initial Data Service

---
+++ src/main/java/ru/vas/dataservice/integration/DataSourceFlow.java
@@ -0,0 +1,12 @@
+package ru.vas.dataservice.integration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.EnableIntegration;
+
+@EnableIntegration
+@Configuration
+@Slf4j
+public class DataSourceFlow {
+
+}
